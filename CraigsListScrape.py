8b860def7807232591bb4c5d112e8b284923e238
Liang <Liang>
2017-05-20 19:19:35 -0400

    more code changes

--- CraigsListScrape.py
+++ CraigsListScrape.py
@@ -1,45 +1,15 @@
-from craigslist import CraigslistHousing
-import shapefile
+# from craigslist import CraigslistHousing
 
-#Comparator to determine location in the neighbourhood box
-#geotag = [latitude, longitude]
-#coords = [bottomleft-long, lat, topright-long, lat]
-def inArea(geotag, coords):
-    if (coords[0] < geotag[1] < coords[2] and 
-        coords[1] < geotag[0] < coords[3]):
-        return True
-    return False
-
-shpf = shapefile.Reader("TorNeighbour.shp")
-
-records = shpf.iterShapeRecords()
-neighbourhoods = {}
-for record in records:
-    name = record.record
-    bbox = record.shape.bbox
-    neighbourhoods[name[1]] = [coord for coord in bbox]
-
-#cl = CraigslistHousing(site='toronto', area='tor', category='apa',
-                       # filters={'max_price': 2000, 'min_price': 1000})
-
-cl = CraigslistHousing(site='toronto', area='tor', category='apa', filters={'max_price': 2000, 'min_price': 1000})
+# cl = CraigslistHousing(site='toronto', area='tor', category='apa', filters={'max_price': 2000, 'min_price': 1000})
                        
-results = cl.get_results(sort_by='newest', geotagged=True, limit=20)
+# results = cl.get_results(sort_by='newest', geotagged=True, limit=20)
+
 
 for result in results:
     geotag = result["geotag"]
     area_found = False
     area = ""
     for aname, acoord in neighbourhoods.items():
-        try:
-            if (inArea(geotag, acoord)):
-                area = aname
-                area_found = True
-                print("Found in " + area)
-                print(result)
-                printI("\n")
-        except:
-            print("Location not found")
-            print(result)
-            printI("\n")
-            
+        
+
+
---
+++ neighbourGen.py
@@ -0,0 +1,31 @@
+import shapefile
+
+class region(object):
+    def __init__(self):
+        self.neighbourhoods = {}
+        self.SHAPEFILE="TorNeighbour.shp"
+        self.initShapeFile()
+
+    #Comparator to determine location in the neighbourhood box
+    #geotag = [latitude, longitude]
+    #coords = [bottomleft-long, lat, topright-long, lat]
+    def inAreaApprox(self, geotag, coords):
+        if (coords[0] < geotag[1] < coords[2] and 
+            coords[1] < geotag[0] < coords[3]):
+            return True
+        return False
+
+    def initShapeFile(self):
+        shpf = shapefile.Reader(self.SHAPEFILE)
+
+        records = shpf.iterShapeRecords()
+        for record in records:
+            name = record.record
+            bbox = record.shape.bbox
+            self.neighbourhoods[name[1]] = [coord for coord in bbox]
+            
+    def findArea(self, geotag):
+        for a, coord in self.neighbourhoods.items():
+            if (self.inAreaApprox(geotag, coord)):
+                return a
+        return "none"
